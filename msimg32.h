5fc493093df5db3252b9efcc3b475d72f8ce27ff
Roman Dudin <r.dudin@drweb.com>
2019-05-22 17:05:12 +0300

    remove redundant examples

--- msimg32.h
+++
@@ -1,7 +0,0 @@
-#define DLL_NAME "msimg32"
-#include "hijack.h"
-PLACEHOLDER __place_holder_1() NOP_FUNC(__LINE__);
-PLACEHOLDER __place_holder_2() NOP_FUNC(__LINE__);
-PLACEHOLDER __place_holder_3() NOP_FUNC(__LINE__);
-PLACEHOLDER __place_holder_4() NOP_FUNC(__LINE__);
-PLACEHOLDER __place_holder_5() NOP_FUNC(__LINE__);
